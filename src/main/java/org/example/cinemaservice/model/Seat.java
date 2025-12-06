0a799062c12aeb1aaf9c645c92a799f081479c38
Viktoria <viktoriia.andreeva@gmail.com>
2025-12-06 20:28:45 +0400

    Change GenerationType

--- src/main/java/org/example/cinemaservice/model/Seat.java
+++ src/main/java/org/example/cinemaservice/model/Seat.java
@@ -14,7 +14,7 @@ import lombok.Setter;
 @Table(uniqueConstraints = {@UniqueConstraint(name = "UniqueSeatAndHall", columnNames = {"row", "number", "hall_id"})})
 public class Seat {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
